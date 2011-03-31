0cb244f85cc9818a368425e98d7d538992fd14e6
Eamonn Maguire <eamonnmag@gmail.com>
2011-03-31 15:16:04 +0100

    Final changes to ISAcreator Configurator to all for drag and drop on the Field list in order to move elements around. Added marker to show where fields will be moved to

---
+++ src/main/java/org/isatools/isacreatorconfigurator/common/ReOrderableJList.java
@@ -0,0 +1,12 @@
+package org.isatools.isacreatorconfigurator.common;
+
+/**
+ * Created by the ISA team
+ *
+ * @author Eamonn Maguire (eamonnmag@gmail.com)
+ *         <p/>
+ *         Date: 30/03/2011
+ *         Time: 08:57
+ */
+public class ReOrderableJList {
+}
