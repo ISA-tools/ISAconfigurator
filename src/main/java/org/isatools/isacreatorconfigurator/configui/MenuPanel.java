5e698a3d9819f6a5ee4629efafa021055d43282a
Eamonn Maguire <eamonnmag@gmail.com>
2013-02-22 16:26:52 +0000

    Threading issues resolved in Java 7
    Fixed threading issue in Java 7 which stopped the file chooser appearing and also prevented statuses from being shown etc.

--- src/main/java/org/isatools/isacreatorconfigurator/configui/MenuPanel.java
+++ src/main/java/org/isatools/isacreatorconfigurator/configui/MenuPanel.java
@@ -56,7 +56,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -133,7 +132,6 @@ public class MenuPanel extends JLayeredPane {
             setLayout(new BorderLayout());
             setOpaque(false);
             addMouseListener(this);
-
         }
 
         public void createGUI() {
@@ -180,25 +178,11 @@ public class MenuPanel extends JLayeredPane {
                 public void mousePressed(MouseEvent event) {
                     try {
                         openExistingConfiguration.setIcon(openExistingIcon);
-                        Thread performer = new Thread(new Runnable() {
+                        SwingUtilities.invokeLater(new Runnable() {
                             public void run() {
-
-                                try {
-                                    loadSession();
-                                } catch (IOException e) {
-                                    showErrorInStatus("<html>Invalid selection made. Please choose a valid ISA configuration directory...</html>");
-                                    e.printStackTrace();
-                                } catch (ClassNotFoundException e) {
-                                    showErrorInStatus("<html>The table configuration file you have loaded is incorrectly formed!</html>");
-                                    e.printStackTrace();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                    showErrorInStatus("<html>Invalid selection made. Please choose a valid ISA configuration directory...</html>");
-                                }
+                                loadSession();
                             }
                         });
-                        performer.start();
-
                     } catch (Exception e) {
                         log.error("e " + e.getMessage());
                     }
@@ -220,7 +204,7 @@ public class MenuPanel extends JLayeredPane {
             exit.addMouseListener(new MouseAdapter() {
 
                 public void mousePressed(MouseEvent event) {
-                     System.exit(1);
+                    System.exit(1);
                 }
 
                 public void mouseEntered(MouseEvent event) {
@@ -283,7 +267,7 @@ public class MenuPanel extends JLayeredPane {
      * @throws java.io.IOException    - When file not found
      * @throws ClassNotFoundException - When object being read in from input stream doesn't read in properly
      */
-    private boolean loadSession() throws IOException, ClassNotFoundException, XmlException {
+    private void loadSession()  {
         jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         jfc.setFileFilter(new ConfigurationFileFilter());
         jfc.setDialogTitle("Select configuration file directory");
@@ -297,78 +281,91 @@ public class MenuPanel extends JLayeredPane {
             toLoad = jfc.getSelectedFile();
 
             if (toLoad != null && checkDirectoryContentIsOk(toLoad)) {
-                DataEntryPanel dep = new DataEntryPanel(appCont, toLoad);
 
-
-                SwingUtilities.invokeLater(new Runnable() {
+                Thread loadThread = new Thread(new Runnable() {
                     public void run() {
-                        status.setForeground(UIHelper.DARK_GREEN_COLOR);
-                        status.setText("<html>attempting to load <i>" + toLoad.getName() + "</i></html>");
-                        status.setText("<html>processing files...</html>");
+
+                        try {
+                            DataEntryPanel dep = new DataEntryPanel(appCont, toLoad);
+
+                            SwingUtilities.invokeLater(new Runnable() {
+                                public void run() {
+                                    status.setForeground(UIHelper.DARK_GREEN_COLOR);
+                                    status.setText("<html>attempting to load <i>" + toLoad.getName() + "</i></html>");
+                                    status.setText("<html>processing files...</html>");
+                                }
+                            });
+
+                            // we want to attempt to parse the XML since this directory should contain XML describing configurations
+                            if (toLoad.isDirectory()) {
+
+                                ConfigXMLParser parser = new ConfigXMLParser(ConfigurationLoadingSource.ISACONFIGURATOR, toLoad.getAbsolutePath());
+                                parser.loadConfiguration();
+                                Configuration c = new Configuration(parser.getMappings());
+                                for (TableReferenceObject tc : parser.getTables()) {
+                                    c.addTableObject(tc.getTableFields());
+                                }
+
+                                SwingUtilities.invokeLater(new Runnable() {
+                                    public void run() {
+                                        status.setText("<html>connecting to ontology resources...</html>");
+                                    }
+                                });
+
+                                loadTFO(c, dep);
+
+                            }
+                            // otherwise, this is a legacy TFO or TCO file.
+                            else {
+
+                                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(
+                                        toLoad));
+
+                                if (ConfigurationFileFilter.getExtension(toLoad).equalsIgnoreCase("tfo")) {
+                                    Configuration c = (Configuration) ois.readObject();
+                                    ois.close();
+                                    SwingUtilities.invokeLater(new Runnable() {
+                                        public void run() {
+                                            status.setText("<html>connecting to ontology resources...</html>");
+                                        }
+                                    });
+                                    loadTFO(c, dep);
+                                } else {
+                                    log.info("This format is not supported...");
+                                    showErrorInStatus("<html>This file format is not supported...</html>");
+                                }
+                            }
+
+                            dep.createGUI();
+                            status.setText("");
+                            status.setIcon(null);
+                            appCont.hideGlassPane();
+                            appCont.setCurrentPage(dep);
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 });
-
-                // we want to attempt to parse the XML since this directory should contain XML describing configurations
-                if (toLoad.isDirectory()) {
-
-                    ConfigXMLParser parser = new ConfigXMLParser(ConfigurationLoadingSource.ISACONFIGURATOR, toLoad.getAbsolutePath());
-                    parser.loadConfiguration();
-                    Configuration c = new Configuration(parser.getMappings());
-                    for (TableReferenceObject tc : parser.getTables()) {
-                        c.addTableObject(tc.getTableFields());
-                    }
-
-                    SwingUtilities.invokeLater(new Runnable() {
-                        public void run() {
-                            status.setText("<html>connecting to ontology resources...</html>");
-                        }
-                    });
-
-                    loadTFO(c, dep);
-
-                }
-                // otherwise, this is a legacy TFO or TCO file.
-                else {
-
-                    ObjectInputStream ois = new ObjectInputStream(new FileInputStream(
-                            toLoad));
-
-                    if (ConfigurationFileFilter.getExtension(toLoad).equalsIgnoreCase("tfo")) {
-                        Configuration c = (Configuration) ois.readObject();
-                        ois.close();
-                        SwingUtilities.invokeLater(new Runnable() {
-                            public void run() {
-                                status.setText("<html>connecting to ontology resources...</html>");
-                            }
-                        });
-                        loadTFO(c, dep);
-                    } else {
-                        log.info("This format is not supported...");
-                        showErrorInStatus("<html>This file format is not supported...</html>");
-
-                    }
-                }
-
-                dep.createGUI();
-                status.setText("");
-                status.setIcon(null);
-                appCont.hideGlassPane();
-                appCont.setCurrentPage(dep);
-                return true;
-
+                loadThread.start();
             } else {
                 showErrorInStatus("<html>Invalid selection made. Please choose a valid ISA configuration directory...</html>");
             }
+        } else {
+            showErrorInStatus("<html>Invalid selection made. Please choose a valid ISA configuration directory...</html>");
         }
-        showErrorInStatus("<html>Invalid selection made. Please choose a valid ISA configuration directory...</html>");
 
-        return false;
     }
 
-    private void showErrorInStatus(String message) {
-        status.setForeground(UIHelper.RED_COLOR);
-        status.setIcon(null);
-        status.setText(message);
+    private void showErrorInStatus(final String message) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                status.setForeground(UIHelper.RED_COLOR);
+                status.setIcon(null);
+                status.setText(message);
+            }
+        });
     }
 
 
@@ -408,8 +405,8 @@ public class MenuPanel extends JLayeredPane {
     private boolean checkDirectoryContentIsOk(File directory) {
         File[] files = directory.listFiles();
 
-        for(File file : files) {
-            if(ConfigurationFileFilter.getExtension(file).equalsIgnoreCase("xml")) {
+        for (File file : files) {
+            if (ConfigurationFileFilter.getExtension(file).equalsIgnoreCase("xml")) {
                 return true;
             }
         }
